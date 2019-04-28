a73889ae7766398bb92bcf5fdbf29c920f825e79
quangnghiauit <quangnghiauit@gmail.com>
2019-04-28 16:11:47 +0700

    front end

--- webapp/src/components/Sidebar/_nav.js
+++ webapp/src/components/Sidebar/_nav.js
@@ -12,25 +12,25 @@ export default {
         {
             name : "QUẢN LÝ KHÁCH HÀNG",
             url:"/user-management",
-            icon: "icon-speedometer",
+            icon: "icon-puzzle",
             // roles:["ADMIN"],
             children: [
                 {
                     name: "Tra cứu khách hàng",
                     url:"/user-management/search",
-                    icon: "",
+                    icon: "icon-puzzle",
                     // roles: ["ADMIN"]
                 },
                 {
                     name: "Thông tin khách hàng",
                     url :"/user-management/user-info",
-                    icon:"",
+                    icon:"icon-puzzle",
                     // roles: ["ADMIN"]
                 },
                 {
                     name: "Lịch sử giao dịch",
                     url :"/user-management/history",
-                    icon:"",
+                    icon:"icon-puzzle",
                     // roles: ["ADMIN"]
                 }
             ]
@@ -38,25 +38,25 @@ export default {
         {
             name : "QUẢN LÝ XE",
             url:"/car-management",
-            icon: "icon-speedometer",
+            icon: "icon-star",
             // roles:["ADMIN"],
             children: [
                 {
                     name: "Tra cứu xe",
                     url:"/car-management/search",
-                    icon: "",
+                    icon: "icon-star",
                     // roles: ["ADMIN"]
                 },
                 {
                     name: "Thông tin xe xử lý",
                     url :"/car-management/car-handle-info",
-                    icon:"",
+                    icon:"icon-star",
                     // roles: ["ADMIN"]
                 },
                 {
                     name: "Xử lý xe",
                     url :"/car-management/car-handle-list",
-                    icon:"",
+                    icon:"icon-star",
                     // roles: ["ADMIN"]
                 }
             ]
@@ -64,19 +64,19 @@ export default {
         {
             name : "QUẢN LÝ PHỤ TÙNG",
             url:"/material-management",
-            icon: "icon-speedometer",
+            icon: "icon-energy",
             // roles:["ADMIN"],
             children: [
                 {
                     name: "Tra cứu phụ tùng",
                     url:"/material-management/search",
-                    icon: "",
+                    icon: "icon-energy",
                     // roles: ["ADMIN"]
                 },
                 {
                     name: "Lịch sử nhập phụ tùng",
                     url :"/material-management/history",
-                    icon:"",
+                    icon:"icon-energy",
                     // roles: ["ADMIN"]
                 }
             ]
@@ -84,19 +84,19 @@ export default {
         {
             name : "QUẢN LÝ HÓA ĐƠN",
             url:"/bill-management",
-            icon: "icon-speedometer",
+            icon: "icon-note",
             // roles:["ADMIN"],
             children: [
                 {
                     name: "Lập hóa đơn",
                     url:"/bill-management/create-bill",
-                    icon: "",
+                    icon: "icon-note",
                     // roles: ["ADMIN"]
                 },
                 {
                     name: "Tra cứu hóa đơn",
                     url :"/bill-management/history",
-                    icon:"",
+                    icon:"icon-note",
                     // roles: ["ADMIN"]
                 }
             ]
@@ -104,19 +104,19 @@ export default {
         {
             name : "BÁO CÁO",
             url:"/report-management",
-            icon: "icon-speedometer",
+            icon: "icon-calculator",
             // roles:["ADMIN"],
             children: [
                 {
                     name: "Lập báo cáo tồn",
                     url:"/report-management/create-report-inventory",
-                    icon: "",
+                    icon: "icon-calculator",
                     // roles: ["ADMIN"]
                 },
                 {
                     name: "Lập báo cáo doanh thu",
                     url :"/report-management/create-report-revenue",
-                    icon:"",
+                    icon:"icon-calculator",
                     // roles: ["ADMIN"]
                 }
             ]
@@ -124,19 +124,19 @@ export default {
         {
             name : "ADMIN",
             url:"/admin-management",
-            icon: "icon-speedometer",
+            icon: "icon-layers",
             // roles:["ADMIN"],
             children: [
                 {
                     name: "Quản lý user",
                     url:"/admin-management/user-management",
-                    icon: "",
+                    icon: "icon-layers",
                     // roles: ["ADMIN"]
                 },
                 {
                     name: "Quản lý roles",
                     url :"/admin-management/role-management",
-                    icon:"",
+                    icon:"icon-layers",
                     // roles: ["ADMIN"]
                 }
             ]
