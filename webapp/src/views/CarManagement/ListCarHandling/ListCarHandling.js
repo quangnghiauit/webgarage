4486d0a095e517b79ea7c2fae7f5c56838f1685c
tnd4698 <tnd4698@gmail.com>
2019-06-04 21:24:41 +0700

    them pagination danh sach xe dang xu ly

--- webapp/src/views/CarManagement/ListCarHandling/ListCarHandling.js
+++ webapp/src/views/CarManagement/ListCarHandling/ListCarHandling.js
@@ -1,6 +1,4 @@
 import React, {Component} from 'react';
-import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
-import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
 import {
     Card,
     CardHeader,
@@ -16,7 +14,11 @@ import {
     Input,
     FormGroup,
     Form,
-    Label, Badge
+    Label, Badge,
+    Pagination,
+    PaginationItem,
+    PaginationLink,
+    InputGroup
 } from 'reactstrap';
 import {getInfoMaterialUser} from "../../../api/TransManagement/transmanagement";
 import {getListCarHandling} from "../../../api/CarManagement/carmanagement";
@@ -28,13 +30,25 @@ class ListCarHandling extends Component {
         super(props);
         this.state = {
             list: [],
+            collapseCar:false, 
+            modalAddMaterials:false,
+
+            curPaItem:1,
+            maxRows: 10,
+            maxPaItems: 3,
+            definePa:[],
+            filterPa:[]
         }
 
-        this.state={ collapseCar:false, modalAddMaterials:false };
         this.toggleCar=this.toggleCar.bind(this);
         this.toggleMaterials=this.toggleMaterials.bind(this);
 
         this.handleGetListCarHandling = this.handleGetListCarHandling.bind(this);
+
+        this.filterPa=this.filterPa.bind(this);
+        this.togglePa=this.togglePa.bind(this);
+        this.toggleNext=this.toggleNext.bind(this);
+        this.togglePre=this.togglePre.bind(this);
     }
 
     componentDidMount() {
@@ -46,9 +60,150 @@ class ListCarHandling extends Component {
             this.setState({
                 listTable: response.data,
                 list: response.data,
+            },()=>{
+                const table=document.getElementById('table-cars-handling');
+                const tr=table.getElementsByTagName('tr');
+                if(tr.length-1>this.state.maxRows)
+                {
+                    let temp=[];
+                    for(let i=1;i<=Math.ceil((tr.length-1)/this.state.maxRows);i++)
+                        temp.push(i);
+                    this.setState({definePa:temp},
+                        ()=>{
+                            if(this.state.definePa.length-this.state.curPaItem+1>=this.state.maxPaItems)
+                            {
+                                let temp=[];
+                                for(let i=this.state.curPaItem-1;i<this.state.curPaItem+this.state.maxPaItems-1;i++)
+                                {
+                                    temp.push(this.state.definePa[i]);
+                                }
+                                this.setState({filterPa:temp});
+                            }
+                            else
+                            {
+                                let temp=[];
+                                if(this.state.definePa.length-this.state.maxPaItems>=0)
+                                    for(let i=this.state.definePa.length-this.state.maxPaItems;i<this.state.definePa.length;i++)
+                                        temp.push(this.state.definePa[i]);
+                                else{
+                                    temp=[...this.state.definePa];
+                                }
+                                this.setState({filterPa:temp});
+                            }
+                        });
+                }
+                else
+                    this.setState({definePa:[1]},
+                        ()=>{
+                            this.setState({filterPa:this.state.definePa});
+                        });
+                this.filterPa();
             })
 
         })
+    }
+
+    filterTable(){
+        let td,txtValue,display;
+        const filter = document.getElementById("search").value.toUpperCase();
+        const table = document.getElementById("table-cars-handling");
+        const tr = table.getElementsByTagName("tr");
+        for (let i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td");
+            display=false;
+            for(let j=0;j<td.length;j++){
+                txtValue = td[j].textContent || td[j].innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1)
+                {
+                    display=true;
+                    break;
+                }
+            }
+            if (display) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+
+        }
+    }
+
+    filterPa(){
+        const table=document.getElementById('table-cars-handling');
+        const tr=table.getElementsByTagName('tr');
+        for(let i=1;i<tr.length;i++)
+        {
+            if((i>=(this.state.curPaItem-1)*this.state.maxRows+1) && (i<=this.state.curPaItem*this.state.maxRows))
+                tr[i].style.display='';
+            else
+                tr[i].style.display='none';
+        }
+    }
+    togglePre(){
+        if(this.state.curPaItem > 1)
+        {
+            this.setState({
+                curPaItem:this.state.curPaItem-1
+            },()=>{
+                this.filterPa();
+                if(this.state.definePa.length-this.state.curPaItem+1>=this.state.maxPaItems)
+                {
+                    let temp=[];
+                    for(let i=this.state.curPaItem-1;i<this.state.curPaItem+this.state.maxPaItems-1;i++)
+                    {
+                        temp.push(this.state.definePa[i]);
+                    }
+                    this.setState({filterPa:temp});
+                }
+                else
+                {
+                    let temp=[];
+                    if(this.state.definePa.length-this.state.maxPaItems>=0)
+                        for(let i=this.state.definePa.length-this.state.maxPaItems;i<this.state.definePa.length;i++)
+                            temp.push(this.state.definePa[i]);
+                    else{
+                        temp=[...this.state.definePa];
+                    }
+                    this.setState({filterPa:temp});
+                }
+            });
+        }
+    }
+    toggleNext(){
+        if(this.state.curPaItem*this.state.maxRows<this.state.list.length)
+        {
+            this.setState({
+                curPaItem:this.state.curPaItem+1
+            },()=>{
+                this.filterPa();
+                if(this.state.definePa.length-this.state.curPaItem+1>=this.state.maxPaItems)
+                {
+                    let temp=[];
+                    for(let i=this.state.curPaItem-1;i<this.state.curPaItem+this.state.maxPaItems-1;i++)
+                    {
+                        temp.push(this.state.definePa[i]);
+                    }
+                    this.setState({filterPa:temp});
+                }
+                else
+                {
+                    let temp=[];
+                    if(this.state.definePa.length-this.state.maxPaItems>=0)
+                        for(let i=this.state.definePa.length-this.state.maxPaItems;i<this.state.definePa.length;i++)
+                            temp.push(this.state.definePa[i]);
+                    else{
+                        temp=[...this.state.definePa];
+                    }
+                    this.setState({filterPa:temp});
+                }
+            });
+        }
+    }
+    togglePa(i){
+        this.setState({
+            curPaItem:i
+        },()=>{this.filterPa()}
+        );
     }
 
     handleCar(id) {
@@ -63,6 +218,17 @@ class ListCarHandling extends Component {
     }
     render() {
         const {list} = this.state;
+        const listPaItems=this.state.filterPa.map((i,index)=>
+            this.state.curPaItem===i?
+                <PaginationItem key={index} active id={'paItem'+i}>
+                    <PaginationLink onClick={()=>this.togglePa(i)}>{i}</PaginationLink>
+                </PaginationItem>
+                :
+                <PaginationItem key={index} id={'paItem'+i}>
+                    <PaginationLink onClick={()=>this.togglePa(i)}>{i}</PaginationLink>
+                </PaginationItem>
+
+        );
         return (
             <div className="animated search-car-handling">
                 <Card>
@@ -70,7 +236,13 @@ class ListCarHandling extends Component {
                         <i className="icon-menu"></i>Danh sách xe đang xử lý
                     </CardHeader>
                     <CardBody>
-                        <Table responsive striped>
+                        <InputGroup className="search">
+                            <Input type="text" id="search" onKeyUp={this.filterTable} placeholder="Search..." title="Enter a search info" />
+                            <div className="input-group-append">
+                                <i className="fa fa-search form-control" aria-hidden="true"></i>
+                            </div>
+                        </InputGroup>
+                        <Table id="table-cars-handling" responsive striped>
                             <thead>
                             <tr>
                                 <th>ID</th>
@@ -114,6 +286,15 @@ class ListCarHandling extends Component {
                             }
                             </tbody>
                         </Table>
+                        <Pagination id="pagination">
+                            <PaginationItem>
+                                <PaginationLink previous onClick={this.togglePre}/>
+                            </PaginationItem>
+                            {listPaItems}
+                            <PaginationItem>
+                                <PaginationLink next onClick={this.toggleNext}/>
+                            </PaginationItem>
+                        </Pagination>
                     </CardBody>
                 </Card>
                 <Collapse isOpen={this.state.collapseCar}>
@@ -145,7 +326,7 @@ class ListCarHandling extends Component {
                             </Modal>
                         </CardHeader>
                         <CardBody>
-                            <Table responsive>
+                            <Table responsive >
                                 <thead>
                                 <tr>
                                     <th>Phụ tùng</th>
